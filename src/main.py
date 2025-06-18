73b549e1fe701e2b0ee6a466ed8afb6df9105a3d
Den-is-Ka <D_K81@mail.ru>
2025-06-18 15:07:04 +0500

    лясим трясим

--- src/main.py
+++ src/main.py
@@ -7,3 +7,10 @@ def divide(a, b):
 
 def calculate_logarithm(x, base):
     math.log(x, base)
+
+    #  тут я как бы пишу код для взлома пентагона и комичу его
+
+if __name__ == "__main__":
+    # чтото я тут подобавлял и лясим трясим дед Герасим
+    print(calculate_logarithm(4, 8))
+
